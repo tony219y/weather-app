3a9c2156686e7dee9b4e4420265de78267e31f2d
tony007x <aekanan2004@gmail.com>
2025-02-13 15:36:15 +0700

    test-api-get

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,13 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
 
+app.get("/", () => {
+  return "Hello World";
+});
+
+
+app.listen(3000);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
